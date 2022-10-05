e96a7eebd02751ff40407c328d1282e8d57c3a69
Avery Harrah <harrahawofficial@gmail.com>
2022-10-05 02:13:35 -0500

    Attempting to Use New Tweening Method

--- BlindFighterProject/Player/Player.gd
+++ BlindFighterProject/Player/Player.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 
 var starting_position : Vector2
+var dodge_distance : int = 300
 
 onready var animation_player = $AnimationPlayer
 
@@ -13,3 +14,18 @@ func _ready():
 func _process(_delta):
 	pass
 
+
+func move(location : String = "start") -> void:
+	match location:
+		"left":
+			var tween := create_tween()
+			tween.tween_property(self, "global_position.x", starting_position.x - dodge_distance, animation_player.current_animation_length)
+		"right":
+			var tween := create_tween()
+			tween.tween_property(self, "global_position.x", starting_position.x + dodge_distance, animation_player.current_animation_length)
+		"start":
+			var tween := create_tween()
+			tween.tween_property(self, "global_position", starting_position, animation_player.current_animation_length)
+		_:
+			print("Error: Incorrect location given for move() function")
+			
