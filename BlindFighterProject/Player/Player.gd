f382932274d902c1f9cec8879881efce9a2d2d4f
Avery Harrah <harrahawofficial@gmail.com>
2022-10-05 01:24:24 -0500

    Added the Animation Ended Signal and Transition Boolians to the State Machine

--- BlindFighterProject/Player/Player.gd
+++ BlindFighterProject/Player/Player.gd
@@ -1,6 +1,14 @@
 extends Node2D
 
+
+var starting_position : Vector2
+
 onready var animation_player = $AnimationPlayer
+
+
+func _ready():
+	starting_position = global_position
+
 
 func _process(_delta):
 	pass
--- BlindFighterProject/Player/PlayerStateMachine.gd
+++ BlindFighterProject/Player/PlayerStateMachine.gd
@@ -1,6 +1,12 @@
 extends "res://Scripts/StateMachine.gd"
 
 
+# Variables to tell if the state should transition to another specific state
+var transition_to_idle : bool = false
+var transition_to_return_left : bool = false
+var transition_to_return_right : bool = false
+
+
 func _ready():
 	add_state("idle")
 	add_state("dodge_left")
@@ -10,7 +16,7 @@ func _ready():
 	#add_state("attack")
 	#add_state("parry")
 	#add_state("stun") # State for when the player is hit by an attack
-	call_deferred("set_state", states.idle)
+	call_deferred("set_state", states.idle) # Sets Idle as the initial state
 
 
 func _state_logic(delta):
@@ -23,29 +29,39 @@ func _get_transition(delta):
 		states.idle:
 			#if Input.is_action_just_pressed("attack"):
 				#return states.attack
+
 			#if Input.is_action_just_pressed("parry"):
 				#return states.parry
+
 			if Input.is_action_just_pressed("dodge_left"):
 				return states.dodge_left
+
 			if Input.is_action_just_pressed("dodge_right"):
 				return states.dodge_right
 
 
-# Function to place one-shot code that activates on a state being entered, like tweening and timers
 func _enter_state(new_state, old_state):
 	match new_state:
 		states.idle:
 			parent.animation_player.play("idle")
+
 		#states.attack:
 			#parent.animation_player.play("attack")
+
 		#states.parry:
 			#parent.animation_player.play("parry")
+
 		states.dodge_left:
 			parent.animation_player.play("dodge_left")
+			
+
 		states.dodge_right:
 			parent.animation_player.play("dodge_right")
+
+		# Returning from the left requeres jumping to the right and vice versa
 		states.return_left:
 			parent.animation_player.play("dodge_right")
+
 		states.return_right:
 			parent.animation_player.play("dodge_left")
 
@@ -53,3 +69,13 @@ func _enter_state(new_state, old_state):
 # Function to place one-shot code on exiting a state
 func _exit_state(old_state, new_state):
 	pass
+
+
+func _on_AnimationPlayer_animation_finished(anim_name):
+	match anim_name:
+		"dodge_left":
+			transition_to_return_left = true
+		"dodge_right":
+			transition_to_return_right = true
+		"return_left", "return_right":
+			transition_to_idle = true
