cdc9c6bbb3473875fce26d3afd9605e4ecd8b678
Avery Harrah <harrahawofficial@gmail.com>
2022-10-05 10:34:24 -0500

    Implemented Tween for Dodging

--- BlindFighterProject/Player/Player.gd
+++ BlindFighterProject/Player/Player.gd
@@ -2,13 +2,13 @@ extends Node2D
 
 
 var starting_position : Vector2
-var dodge_distance : int = 300
+var dodge_distance : int = 400
 
 onready var animation_player = $AnimationPlayer
 
 
 func _ready():
-	starting_position = global_position
+	starting_position = $SpriteContainer.get_position()
 
 
 func _process(_delta):
@@ -18,14 +18,14 @@ func _process(_delta):
 func move(location : String = "start") -> void:
 	match location:
 		"left":
-			var tween := create_tween()
-			tween.tween_property(self, "global_position.x", starting_position.x - dodge_distance, animation_player.current_animation_length)
+			var tween := create_tween().set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_OUT)
+			tween.tween_property($SpriteContainer, "position", Vector2(starting_position.x - dodge_distance, starting_position.y), animation_player.current_animation_length)
 		"right":
-			var tween := create_tween()
-			tween.tween_property(self, "global_position.x", starting_position.x + dodge_distance, animation_player.current_animation_length)
+			var tween := create_tween().set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_OUT)
+			tween.tween_property($SpriteContainer, "position", Vector2(starting_position.x + dodge_distance, starting_position.y), animation_player.current_animation_length)
 		"start":
-			var tween := create_tween()
-			tween.tween_property(self, "global_position", starting_position, animation_player.current_animation_length)
+			var tween := create_tween().set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_OUT)
+			tween.tween_property($SpriteContainer, "position", starting_position, animation_player.current_animation_length)
 		_:
 			print("Error: Incorrect location given for move() function")
 			
