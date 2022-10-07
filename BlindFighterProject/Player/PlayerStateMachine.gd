bb592309ffe5c8fa3c515a4be099fcc258322051
Avery Harrah <harrahawofficial@gmail.com>
2022-10-07 14:01:17 -0500

    Added Attack to State Machine

--- BlindFighterProject/Player/PlayerStateMachine.gd
+++ BlindFighterProject/Player/PlayerStateMachine.gd
@@ -9,12 +9,12 @@ var transition_to_return_right : bool = false
 
 func _ready():
 	add_state("idle")
+	add_state("attack")
+	#add_state("parry")
 	add_state("dodge_left")
 	add_state("dodge_right")
 	add_state("return_left") # Return states for coming back after dodging
 	add_state("return_right")
-	#add_state("attack")
-	#add_state("parry")
 	#add_state("stun") # State for when the player is hit by an attack
 	call_deferred("set_state", states.idle) # Sets Idle as the initial state
 
@@ -27,8 +27,8 @@ func _state_logic(delta):
 func _get_transition(delta):
 	match state:
 		states.idle:
-			#if Input.is_action_just_pressed("attack"):
-				#return states.attack
+			if Input.is_action_just_pressed("attack"):
+				return states.attack
 
 			#if Input.is_action_just_pressed("parry"):
 				#return states.parry
@@ -38,6 +38,11 @@ func _get_transition(delta):
 
 			if Input.is_action_just_pressed("dodge_right"):
 				return states.dodge_right
+		
+		states.attack:
+			if transition_to_idle == true:
+				transition_to_idle = false
+				return states.idle
 		
 		states.dodge_left:
 			if transition_to_return_left == true:
@@ -65,8 +70,8 @@ func _enter_state(new_state, old_state):
 		states.idle:
 			parent.animation_player.play("idle")
 
-		#states.attack:
-			#parent.animation_player.play("attack")
+		states.attack:
+			parent.animation_player.play("attack")
 
 		#states.parry:
 			#parent.animation_player.play("parry")
@@ -96,6 +101,9 @@ func _exit_state(old_state, new_state):
 
 func _on_AnimationPlayer_animation_finished(anim_name):
 	match anim_name:
+		"attack":
+			if state == states.attack:
+				transition_to_idle = true
 		"dodge_left":
 			if state == states.dodge_left:
 				transition_to_return_left = true
