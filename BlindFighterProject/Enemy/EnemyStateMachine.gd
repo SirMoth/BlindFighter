1fecd95287636bda960d19b1b4e8034401f25b49
Avery Harrah <harrahawofficial@gmail.com>
2022-10-27 01:23:54 -0500

    Added Center Attack State

--- BlindFighterProject/Enemy/EnemyStateMachine.gd
+++ BlindFighterProject/Enemy/EnemyStateMachine.gd
@@ -5,14 +5,17 @@ extends "res://Scripts/StateMachine.gd"
 var transition_to_idle : bool = false
 var transition_to_attack_left : bool = false
 var transition_to_attack_right : bool = false
+var transition_to_attack_center : bool = false
 
 
 func _ready():
 	add_state("idle")
 	add_state("windup_left")
-	add_state("windup_right")
 	add_state("attack_left")
+	add_state("windup_right")
 	add_state("attack_right")
+	add_state("windup_center")
+	add_state("attack_center")
 	call_deferred("set_state", states.idle) # Sets Idle as the initial state
 
 
@@ -35,15 +38,25 @@ func _get_transition(delta):
 				transition_to_attack_left = false
 				return states.attack_left
 		
+		states.attack_left:
+			if transition_to_idle == true:
+				transition_to_idle = false
+				return states.idle
+		
 		states.windup_right:
 			if transition_to_attack_right == true:
 				transition_to_attack_right = false
 				return states.attack_right
 		
-		states.attack_left:
+		states.attack_right:
 			if transition_to_idle == true:
 				transition_to_idle = false
 				return states.idle
+		
+		states.windup_center:
+			if transition_to_attack_center == true:
+				transition_to_attack_center = false
+				return states.attack_center
 		
 		states.attack_right:
 			if transition_to_idle == true:
@@ -59,14 +72,20 @@ func _enter_state(new_state, old_state):
 		states.windup_left:
 			parent.animation_player.play("windup_left")
 
-		states.windup_right:
-			parent.animation_player.play("windup_right")
-
 		states.attack_left:
 			parent.animation_player.play("attack_left")
 
+		states.windup_right:
+			parent.animation_player.play("windup_right")
+
 		states.attack_right:
 			parent.animation_player.play("attack_right")
+
+		states.windup_center:
+			parent.animation_player.play("windup_center")
+
+		states.attack_center:
+			parent.animation_player.play("attack_center")
 
 
 # Function to place one-shot code on exiting a state
@@ -82,14 +101,22 @@ func _on_AnimationPlayer_animation_finished(anim_name):
 			if state == states.windup_left:
 				transition_to_attack_left = true
 
-		"windup_right":
-			if state == states.windup_right:
-				transition_to_attack_right = true
-
 		"attack_left":
 			if state == states.attack_left:
 				transition_to_idle = true
 
+		"windup_right":
+			if state == states.windup_right:
+				transition_to_attack_right = true
+
 		"attack_right":
 			if state == states.attack_right:
+				transition_to_idle = true
+
+		"windup_center":
+			if state == states.windup_center:
+				transition_to_attack_center = true
+
+		"attack_center":
+			if state == states.attack_center:
 				transition_to_idle = true
