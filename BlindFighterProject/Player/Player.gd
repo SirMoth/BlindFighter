efa011588c70466593c8e4e26c2883225d861868
Avery Harrah <harrahawofficial@gmail.com>
2022-10-30 20:08:13 -0500

    Added Sound Effect Player Function to Player

--- BlindFighterProject/Player/Player.gd
+++ BlindFighterProject/Player/Player.gd
@@ -3,12 +3,16 @@ extends Node2D
 
 var starting_position : Vector2
 var dodge_distance : int = 400
+var audioFiles = {}
 
 onready var animation_player = $AnimationPlayer
 
 
 func _ready():
 	starting_position = $SpriteContainer.get_position()
+	audioFiles["player_attack"] = preload("res://Audio/player_attack.wav")
+	audioFiles["player_dodge"] = preload("res://Audio/player_dodge.wav")
+	audioFiles["player_return"] = preload("res://Audio/player_return.wav")
 
 
 func _process(_delta):
@@ -28,4 +32,14 @@ func move(location : String = "start") -> void:
 			tween.tween_property($SpriteContainer, "position", starting_position, animation_player.current_animation_length)
 		_:
 			print("Error: Incorrect location given for move() function")
-			
+
+
+func play_sound_effect(sound_effect : String) -> void:
+	var sound_effect_player = AudioStreamPlayer2D.new()
+	add_child($AudioPlayerContainer)
+	sound_effect_player.stream = audioFiles[sound_effect]
+	sound_effect_player.play()
+	
+	yield(sound_effect_player, "finished")
+	remove_child(sound_effect_player)
+	print("Sound Effect Player Removed")
