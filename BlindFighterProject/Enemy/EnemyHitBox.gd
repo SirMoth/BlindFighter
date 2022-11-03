3a6b10415b91db8997e5595317e60eef9d265d80
Avery Harrah <harrahawofficial@gmail.com>
2022-11-03 00:44:50 -0500

    Implemented Enemy Attack Hitboxes

--- BlindFighterProject/Enemy/EnemyHitBox.gd
+++ BlindFighterProject/Enemy/EnemyHitBox.gd
@@ -19,12 +19,12 @@ func set_location(location):
 	match location:
 		Location.LEFT:
 			if collision.position != Vector2(704, 778):
-				collision.position = Vector2(960, 778)
+				collision.position = Vector2(704, 778)
 			if collision.scale.x != 1:
 				collision.scale.x = 1
 		Location.RIGHT:
 			if collision.position != Vector2(1216, 778):
-				collision.position = Vector2(960, 778)
+				collision.position = Vector2(1216, 778)
 			if collision.scale.x != 1:
 				collision.scale.x = 1
 		Location.CENTER:
@@ -34,12 +34,12 @@ func set_location(location):
 				collision.scale.x = 1
 		Location.LEFT_CENTER:
 			if collision.position != Vector2(832, 778):
-				collision.position = Vector2(960, 778)
+				collision.position = Vector2(832, 778)
 			if collision.scale.x != 1:
 				collision.scale.x = 1
 		Location.RIGHT_CENTER:
 			if collision.position != Vector2(1088, 778):
-				collision.position = Vector2(960, 778)
+				collision.position = Vector2(1088, 778)
 			if collision.scale.x != 1:
 				collision.scale.x = 1
 		Location.ALL:
