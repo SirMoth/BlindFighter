a97dd432c8d2d899e8a8c1c380243348df3d2af6
Avery Harrah <harrahawofficial@gmail.com>
2022-12-02 00:35:40 -0600

    Update EnemyAI.gd

--- BlindFighterProject/Enemy/EnemyAI.gd
+++ BlindFighterProject/Enemy/EnemyAI.gd
@@ -12,53 +12,21 @@ var combo : int = 0
 
 
 func _ready():
-	yield(get_tree().create_timer(2), "timeout")
-	attack_random_side()
-	yield($"%StateMachine", "attack_finished")
-	escalation += 1
-
-
-func _physics_process(delta):
-	if (combo <= escalation) or (combo <= 6 and escalation >= 6):
-		# Matches escalation to determine attack pattern to use.
-		match escalation:
-			0:
-				attack_random_side()
-			1:
-				attack_random()
-			2, 3, 4, 5:
-				if combo == escalation:
-					attack_center()
-				else:
-					attack_random_side()
-			_:
-				if combo == 6:
-					attack_center()
-				else:
-					attack_random()
-		yield($"%StateMachine", "attack_finished")
-		combo += 1
-	# Resets the combo, increases escalation, and gives a window to be attacked after full attack pattern.
-	if (combo > escalation) or (combo > 6):
-		combo = 0
-		escalation += 1
-		yield(get_tree().create_timer(1), "timeout")
+	yield(get_tree().create_timer(2.5), "timeout")
+	next_attack()
 
 
 # Functions to emit attack signals to the State Machine
-func attack_left(number_of_attacks = 1):
-	for i in range(number_of_attacks):
-		emit_signal("attack_left")
+func attack_left():
+	emit_signal("attack_left")
 
-func attack_right(number_of_attacks = 1):
-	for i in range(number_of_attacks):
-		emit_signal("attack_right")
+func attack_right():
+	emit_signal("attack_right")
 
-func attack_center(number_of_attacks = 1):
-	for i in range(number_of_attacks):
-		emit_signal("attack_left")
+func attack_center():
+	emit_signal("attack_center")
 
-func attack_random(number_of_attacks = 1):
+func attack_random():
 	match (randi() % 3):
 		0:
 			emit_signal("attack_left")
@@ -69,7 +37,7 @@ func attack_random(number_of_attacks = 1):
 		_:
 			print("Random attack error: Out of scope")
 
-func attack_random_side(number_of_attacks = 1):
+func attack_random_side():
 	match (randi() % 2):
 		0:
 			emit_signal("attack_left")
@@ -77,3 +45,43 @@ func attack_random_side(number_of_attacks = 1):
 			emit_signal("attack_right")
 		_:
 			print("Random attack error: Out of scope")
+
+
+func next_attack():
+	if (combo <= escalation) or (combo <= 5 and escalation >= 5):
+		combo += 1
+		# Matches escalation to determine attack pattern to use.
+		match escalation:
+			0:
+				attack_random_side()
+				yield(get_tree().create_timer(0.2), "timeout")
+			1:
+				attack_random()
+				yield(get_tree().create_timer(0.2), "timeout")
+			2, 3, 4:
+				if combo == escalation:
+					attack_center()
+				else:
+					attack_random_side()
+				yield(get_tree().create_timer(0.2), "timeout")
+			5:
+				if combo == escalation:
+					attack_center()
+				else:
+					attack_random()
+				yield(get_tree().create_timer(0.2), "timeout")
+			_:
+				if combo == 5:
+					attack_center()
+				else:
+					attack_random()
+	# Resets the combo, increases escalation, and gives a window to be attacked after full attack pattern.
+	if (combo >= escalation) or (combo >= 5):
+		combo = 0
+		escalation += 1
+
+
+func _on_StateMachine_attack_finished():
+	if combo == 0:
+		yield(get_tree().create_timer(1.5), "timeout")
+	next_attack()
