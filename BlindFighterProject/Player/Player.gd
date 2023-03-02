557a70d3da7735e9d799cc9c780c6f04ea330434
Avery Harrah <harrahawofficial@gmail.com>
2023-03-02 10:55:42 -0600

    Balance Changes
    Lowered music volume when turned on. Lowered player health to 3.

--- BlindFighterProject/Player/Player.gd
+++ BlindFighterProject/Player/Player.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 
-var starting_health : int = 4
+var starting_health : int = 3
 var current_health : int = starting_health
 var starting_position : Vector2
 var dodge_distance : int = 400
