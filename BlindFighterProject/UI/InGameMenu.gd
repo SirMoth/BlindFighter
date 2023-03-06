ac59830b4b67f0e398d3ea139e2c00f7b0122b7f
Avery Harrah <harrahawofficial@gmail.com>
2023-03-05 21:26:17 -0600

    Implemented victory and defeat announcements
    Also fixed a bug where you could lose and win simultaneously

--- BlindFighterProject/UI/InGameMenu.gd
+++ BlindFighterProject/UI/InGameMenu.gd
@@ -1,8 +1,13 @@
 extends Control
 
 
+var audioFiles = {}
+
+
 func _ready():
 	pause_mode = Node.PAUSE_MODE_PROCESS # Allows user to navigate menu when game is paused
+	audioFiles["victory"] = preload("res://Audio/Sound Effects/victory.wav")
+	audioFiles["defeat"] = preload("res://Audio/Sound Effects/defeat.wav")
 
 
 func _on_Player_player_health_changed(new_health):
@@ -10,6 +15,7 @@ func _on_Player_player_health_changed(new_health):
 		yield(get_tree().create_timer(0.18), "timeout")
 		get_tree().paused = true
 		$DefeatLabel.visible = true
+		play_sound_effect("defeat")
 
 
 func _on_Enemy_enemy_health_changed(new_health):
@@ -17,3 +23,15 @@ func _on_Enemy_enemy_health_changed(new_health):
 		yield(get_tree().create_timer(0.15), "timeout")
 		get_tree().paused = true
 		$VictoryLabel.visible = true
+		play_sound_effect("victory")
+
+
+func play_sound_effect(sound_effect : String) -> void:
+	var sound_effect_player = AudioStreamPlayer2D.new()
+	add_child(sound_effect_player)
+	sound_effect_player.volume_db += 1 # Change to a non-static variable
+	sound_effect_player.stream = audioFiles[sound_effect]
+	sound_effect_player.play()
+	
+	yield(sound_effect_player, "finished")
+	remove_child(sound_effect_player)
