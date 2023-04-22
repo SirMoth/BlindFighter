30a609603a62fb0b89a7185ad0ec2e2954e5d69c
Avery Harrah <harrahawofficial@gmail.com>
2023-04-21 19:33:43 -0500

    Health Values Changed

--- BlindFighterProject/Enemy/Enemy.gd
+++ BlindFighterProject/Enemy/Enemy.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 
-var starting_health : int = 20
+var starting_health : int = 15
 var current_health : int = starting_health
 var audioFiles = {}
 
--- BlindFighterProject/Player/Player.gd
+++ BlindFighterProject/Player/Player.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 
-var starting_health : int = 99
+var starting_health : int = 10
 var current_health : int = starting_health
 var starting_position : Vector2
 var dodge_distance : int = 400
